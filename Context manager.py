ac68d08821474abf1451abe558c0cc793c47a8d5
Leonid Rusinov <sifonboroda16@icloud@icloud.com>
2025-10-24 23:23:26 +0500

    New commit

---
+++ Context manager.py
@@ -0,0 +1,29 @@
+class DefenderVector:
+    def __init__(self, __v):
+        self.__v = __v
+
+    def __enter__(self):
+        self.__temp = self.__v[:]     # <-- shallow copy списка
+        return self.__temp
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.__v[:] = self.__temp # <-- присваивание через срез (commit)
+        return False
+
+v1 = [1, 2, 3]
+v2 = [2, 3, 3]
+try:
+    with DefenderVector(v1) as dv:
+        for i, a in enumerate(dv):
+            dv[i] += v2[i]
+except:
+    print('Ошибка')
+
+print(v1)
+
+class Notification:
+    pass
+
+class Scheduler:
+    pass
